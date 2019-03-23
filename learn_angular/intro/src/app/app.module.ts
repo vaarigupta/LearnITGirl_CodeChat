9a85a02e6de3af163c09e719420babf749514dd9
vaarigupta <vaarigupta24@gmail.com>
2019-03-24 00:23:21 +0530

    date component added

--- learn_angular/intro/src/app/app.module.ts
+++ learn_angular/intro/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
