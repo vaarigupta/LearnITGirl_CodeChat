0150847d9151e09ecb568b26446a58c1687d428f
vaarigupta <vaarigupta24@gmail.com>
2019-03-24 02:19:28 +0530

    Data binding - One way

--- learn_angular/intro/src/app/date/date.component.ts
+++ learn_angular/intro/src/app/date/date.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-date',
+  selector: 'date',
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.css']
 })
 export class DateComponent implements OnInit {
 
+  dateval: string = new Date().toDateString();
   constructor() { }
 
   ngOnInit() {
