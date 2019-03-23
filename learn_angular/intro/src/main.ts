cb0ab2ddf59369408e1071f83fdf25c3eaef7428
vaarigupta <vaarigupta24@gmail.com>
2019-03-23 16:30:22 +0530

    new component generated

--- learn_angular/intro/src/main.ts
+++ learn_angular/intro/src/main.ts
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
+//Bootstraping the application with this root module
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // Initializes our angular project
