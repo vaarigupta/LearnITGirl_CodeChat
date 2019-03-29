197cb337ef40e52d2b3f396a4350466f2393dc59
vaarigupta <vaarigupta24@gmail.com>
2019-03-30 03:00:43 +0530

    dashboard component added

--- learn_angular/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ learn_angular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import {HeroesComponent} from './heroes/heroes.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'heroes' , component: HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
