9a18ed3dd8fece763de78ee4ca8b3c1dd9615155
vaarigupta <vaarigupta24@gmail.com>
2019-04-04 21:49:02 +0530

    6th tutorial completed

--- learn_angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ learn_angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -8,19 +9,31 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  heroes : Hero[];
-  
-  constructor(private heroService: HeroService) { 
-      
-  }
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
   ngOnInit() {
     this.getHeroes();
   }
-  //waits for the Observable to emit the array of heroes— which could happen now or several minutes from now. Then subscribe passes the emitted array to the callback, which sets the component's heroes property.
+
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
- }
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
 }
