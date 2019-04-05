a92b4d73d3e1243e912affe2b3fa6ad119c68b69
vaarigupta <vaarigupta24@gmail.com>
2019-04-05 15:13:05 +0530

    register and login page added

--- codechat/src/app/app-routing.module.ts
+++ codechat/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent } from './register/register.component';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '#', component: HomeComponent},
+  {path: 'register' , component : RegisterComponent},
+  {path: 'login' , component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- codechat/src/app/app.module.ts
+++ codechat/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
