f5a9a985e2867253b1ad3ad0c41be89cdccfc793
vaarigupta <vaarigupta24@gmail.com>
2019-04-18 14:16:47 +0530

    added editor

--- codechat/src/app/code/code.component.ts
+++ codechat/src/app/code/code.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { Component, OnInit,Input, ViewChild, ElementRef  } from '@angular/core';
 import * as ace from 'ace-builds'; // ace module ..
 // language package, choose your own 
 import 'ace-builds/src-noconflict/mode-javascript';
@@ -9,6 +9,8 @@ import 'ace-builds/src-noconflict/ext-beautify';
 import {themes} from '../themes';
 import {modes} from '../modes';
 
+
+const INIT_CONTENT = '';
 const THEME = 'ace/theme/monokai';
 const LANG = 'ace/mode/javascript';
 @Component({
@@ -19,24 +21,58 @@ const LANG = 'ace/mode/javascript';
 export class CodeComponent implements OnInit {
   themes =  themes;
   modes = modes;
-  @ViewChild('codeEditor') codeEditorElmRef: ElementRef;
+  
   private codeEditor: ace.Ace.Editor;
   private editorBeautify;
+  @ViewChild('codeEditor') codeEditorElmRef: ElementRef;
+  @Input() content: string;
   constructor() { }
 
   ngOnInit () {
     ace.require('ace/ext/language_tools');
       const element = this.codeEditorElmRef.nativeElement;
       const editorOptions = this.getEditorOptions();
+      this.codeEditor = this.createCodeEditor(element, editorOptions);
+        this.setContent(this.content || INIT_CONTENT);
+        // hold reference to beautify extension
       this.editorBeautify = ace.require('ace/ext/beautify');
-      this.codeEditor = ace.edit(element, editorOptions);
-      this.codeEditor.setTheme(THEME);
-      this.codeEditor.getSession().setMode(LANG);
-      this.codeEditor.setShowFoldWidgets(true); // for the scope fold feature
       
    }
 
-   public beautifyContent()
+   private createCodeEditor(element: Element, options: any): ace.Ace.Editor {
+    const editor = ace.edit(element, options);
+    editor.setTheme(THEME);
+    editor.getSession().setMode(LANG);
+    editor.setShowFoldWidgets(true);
+    return editor;
+}
+
+
+   private getEditorOptions() : Partial<ace.Ace.EditorOptions> & { enableBasicAutocompletion?: boolean;}{
+    const basicEditorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 50,
+      maxLines: Infinity,
+   };
+   const extraEditorOptions = { enableBasicAutocompletion: true };
+        return Object.assign(basicEditorOptions, extraEditorOptions);
+};
+
+public getContent() {
+  if (this.codeEditor) {
+      const code = this.codeEditor.getValue();
+      return code;
+  }
+}
+
+public setContent(content: string): void {
+  if (this.codeEditor) {
+      this.codeEditor.setValue(content);
+  }
+}
+
+
+public beautifyContent()
    {
      if( this.codeEditor && this.editorBeautify)
      {
@@ -45,20 +81,14 @@ export class CodeComponent implements OnInit {
      }
    }
 
-   private getCode() {
-    const code = this.codeEditor.getValue();
-    console.log(code);
-        }
-   private getEditorOptions() : Partial<ace.Ace.EditorOptions> & { enableBasicAutocompletion?: boolean;}{
-    const basicEditorOptions: Partial<ace.Ace.EditorOptions> = {
-      highlightActiveLine: true,
-      minLines: 50,
-      maxLines: Infinity,
-   };
-   const extraEditorOptions = {
-    enableBasicAutocompletion: true
-};
-const margedOptions = Object.assign(basicEditorOptions, extraEditorOptions);
-        return margedOptions;
-   };
+
+   public OnContentChange(callback: (content: string, delta: ace.Ace.Delta) => void): void {
+    this.codeEditor.on('change', (delta) => {
+        const content = this.codeEditor.getValue();
+        callback(content, delta);
+    });
 }
+
+
+}
+
