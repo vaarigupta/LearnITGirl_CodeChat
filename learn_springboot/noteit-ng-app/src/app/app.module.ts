68491031e4e1143acef94a1f75bbe73ffa2dd57e
vaarigupta <vaarigupta24@gmail.com>
2019-06-01 03:26:55 +0530

    component created

--- learn_springboot/noteit-ng-app/src/app/app.module.ts
+++ learn_springboot/noteit-ng-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotFoundComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
