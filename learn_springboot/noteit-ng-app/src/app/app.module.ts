3f22de0f9c912f0f2ac556731bae799fe85a1eab
vaarigupta <vaarigupta24@gmail.com>
2019-06-06 01:46:52 +0530

    added spring boot

--- learn_springboot/noteit-ng-app/src/app/app.module.ts
+++ learn_springboot/noteit-ng-app/src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NotesComponent } from './notes/notes.component';
+import { Router,RouterModule, Routes} from "@angular/router";
+import {FormsModule } from "@angular/forms";
+const appRoutes :Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +38,8 @@ import { NotesComponent } from './notes/notes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(appRoutes, {enableTracing:true}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
